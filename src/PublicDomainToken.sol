962aac6eec05370f6b865de31ee19db88b6c089d
Kristopher Wile <kristopher.wile@gmail.com>
2025-01-02 18:21:25 -0800

    Remove Ownable inheritance from PublicDomainToken constructor

--- src/PublicDomainToken.sol
+++ src/PublicDomainToken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -20,7 +21,7 @@ interface IPublicDomainToken is IERC20, IERC20Permit, IVotes {
 }
 
 contract PublicDomainToken is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, IPublicDomainToken {
-    constructor(address initialOwner)
+    constructor()
         ERC20("Public Domain Token", "PDoT")
         ERC20Permit("Public Domain Token")
     {}
