272acfb2ef6aca8a8f0c0171f91d756569bc5489
Kristopher Wile <kristopher.wile@gmail.com>
2025-01-03 14:39:02 -0800

    Add Sepolia profile configuration and deployment script for PublicDomainToken

---
+++ scripts/DeployPublicDomainToken.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+import "../src/PublicDomainToken.sol";
+
+contract DeployPublicDomainToken is Script {
+    function setUp() public {}
+    
+    function run() external {
+        uint privateKey = vm.envUint("DEV_PRIVATE_KEY");
+        address account = vm.addr(privateKey);
+
+        console.log("Account", account);
+        // Start broadcasting transactions using the specified profile (Sepolia)
+        vm.startBroadcast();
+
+        // Deploy the PublicDomainToken contract without passing an initial owner
+        PublicDomainToken token = new PublicDomainToken();
+
+        // Optionally, log the deployed contract address
+        emit log_named_address("PublicDomainToken deployed to:", address(token));
+
+        // Stop broadcasting transactions
+        vm.stopBroadcast();
+    }
+}
